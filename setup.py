e94f97d415d159bbfed47ca04ed15adad0e384bb
Andy Baker <andy@andybak.net>
2015-07-29 13:29:13 +0100

    reuse requirements.txt in setup.py so we don't need to manually install requirements

--- setup.py
+++ setup.py
@@ -2,15 +2,20 @@
 
 from distutils.core import setup
 import os
+from pip.req import parse_requirements
 import setuplib
 
 packages, package_data = setuplib.find_packages('scaffolding')
+
+install_reqs = parse_requirements('requirements.txt', session=uuid.uuid1())
+reqs = [str(ir.req) for ir in install_reqs]
 
 setup(
     name='django-scaffolding',
     version='0.2.3',
     author='Simon Baechler',
     author_email='simon@stellanera.com',
+    install_requires=reqs,
     packages=packages,
     package_data=package_data,
     url='https://github.com/sbaechler/django-scaffolding/',
